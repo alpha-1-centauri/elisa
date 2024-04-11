0ba03106b3de7fac4598752cf2a2bf4bf5a77098
Adam <adam.syanda@gmail.com>
2024-04-12 00:03:52 +0100

    Tidying

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ def process_and_download(uploaded_file, title, analyte, N_STD_CURVES, DILUTION_F
 
             # Calculate standard curve statistics
             # std_curve_concs = pd.Series(std_curve_concs[analyte]).astype(float, errors='ignore')
-            std_curve_concs = layout.iloc[:,0].astype(float, errors='ignore')
+            std_curve_concs = layout.iloc[:,0].astype(float, errors='coerce')
             print(layout)
             print(std_curve_concs)
 
